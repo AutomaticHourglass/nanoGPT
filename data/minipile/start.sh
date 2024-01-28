ebb16134da85d1720384dc8a21c4691c55b446cf
unsalgokdag <unsal.gokdag@gmail.com>
2024-01-28 18:54:01 +0100

    working minipile version

---
+++ data/minipile/start.sh
@@ -0,0 +1,2 @@
+wget --continue -O minipile.idx https://huggingface.co/datasets/BlinkDL/minipile-tokenized/resolve/main/rwkv_vocab_v20230424/minipile.idx
+wget --continue -O minipile.bin https://huggingface.co/datasets/BlinkDL/minipile-tokenized/resolve/main/rwkv_vocab_v20230424/minipile.bin
