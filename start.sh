c611ba0c941c2e94daa8e359cadf8dd6336da7b6
unsalgokdag <unsal.gokdag@gmail.com>
2024-01-28 18:41:38 +0100

    add download script

---
+++ start.sh
@@ -0,0 +1,2 @@
+wget --continue -O data/minipile/minipile.idx https://huggingface.co/datasets/BlinkDL/minipile-tokenized/resolve/main/rwkv_vocab_v20230424/minipile.idx
+wget --continue -O data/minipile/minipile.bin https://huggingface.co/datasets/BlinkDL/minipile-tokenized/resolve/main/rwkv_vocab_v20230424/minipile.bin
